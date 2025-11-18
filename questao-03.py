c19fc606748d8d37981eb11134ec37f47ab24c53
Paulo Roberto <salvator76544567@gmail.com>
2025-11-18 20:09:03 -0300

    Adição descrição da proposta!

--- questao-03.py
+++ questao-03.py
@@ -1,3 +1,9 @@
+"""" 
+3 - Criar um código que serve para verificar se uma senha digitada pelo usuário atende a critérios básicos de segurança.
+a - deve ter pelo menos 8 caracteres.
+b - deve conter pelo menos um número.  
+""""
+
 while True: 
     senha = input("Digite a sua senha:")
     
