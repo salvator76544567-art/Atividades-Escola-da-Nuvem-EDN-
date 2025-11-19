616d3d0c8679eb1582fdf8e6910f5806b02eb59d
Paulo Roberto <salvator76544567@gmail.com>
2025-11-19 19:23:51 -0300

    Descricao completa

--- atividades-pratica05/questao-02.py
+++ atividades-pratica05/questao-02.py
@@ -1,4 +1,4 @@
-""""""
+"""2-  Crie uma função que verifique se uma palavra ou frase é um palíndromo (lê-se igual de trás para frente, ignorando espaços e pontuação). Se o resultado é True, responda “Sim”, se o resultado for False, responda “Não”. """
 
 def eh_palindromo(texto):
 
